450f499899c195f176c8808fc5ebea4ee64a8a45
Valentina Parra <leydi.parra2@soy.sena.edu.co>
2025-08-06 07:28:19 -0500

    Se añade boton logout (sin funcion)

--- Prueba-tecnica/front/src/components/navbar.jsx
+++ Prueba-tecnica/front/src/components/navbar.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../styles/navbar.css';
+import logo from '../img/logo.png';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate('/Iniciosesion'); 
+  };
+
   return (
     <nav className="navbar">
-      <div className="navbar-logo">
-        <img src="../img/logoo.png" alt="Logo" />
+      <div className="navbar-left">
+        <div className="navbar-logo">
+         <img src={logo} alt="Logo" />
+        </div>
+        <h1 className="navbar-title">Gestión de Actas - Valentina Parra</h1>
       </div>
-      <h1 className="navbar-title">Gestión de Actas - Valentina Parra</h1>
+
+      <div className="navbar-right">
+        <button className="logout-button" onClick={handleLogout}>
+          Salir
+        </button>
+      </div>
     </nav>
   );
 };
